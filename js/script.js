db925c74bbdb63b52462f8c7cc56f09990d6473e
Moresop <gardner_christopher@wheatoncollege.edu>
2022-10-31 20:14:08 -0400

    saveing works, working on color stuf

--- js/script.js
+++ js/script.js
@@ -1,15 +1,57 @@
-const storageInput = document.querySelector('.storage');
-const text = document.querySelector('.text');
-const button = document.querySelector('.button');
+var displayCurrentDay = document.querySelector("#currentDay");
+var currentDay = moment();
+displayCurrentDay.textContent = currentDay.format("dddd, MMMM Do YYYY");
+var timeBlock = $(".time-block").addClass("row");
+var blockText = $("<p>").addClass("description");
+timeBlock.append(blockText);
+var currentHour = parseInt(moment().format("H"));
 
-storageInput.addEventListener('input', letter => {
-    text.textConent = letter.target.value
+var loadEvents = function (timeSlots) {
+  timeSlots.forEach((element) => {
+    console.log(element);
+    let text = localStorage.getItem(parseInt(element.time));
+    console.log(text);
+    if (text) {
+      element.text.val(text);
+    }
+  });
+};
 
-})
-const saveToLocalStorage= () => {
-    localStorage.setItem('textinput', text.textContent)
-}
+var fetchEvents = function () {
+  var tempArr = [];
+  $("textarea").each(function (index, elem) {
+    tempArr.push({
+      time: $(elem).attr("id"),
+      text: $(elem),
+    });
+  });
+  loadEvents(tempArr);
+};
 
-button.addEventListener('click', saveToLocalStorage)
+
+//color code will go here
+
+
+$("button.saveBtn").click(function (event, loadEvents) {
+  event.preventDefault();
+  var $element = $(this).siblings("textarea");
+  var time = $element.attr("id");
+  console.log(time);
+  var text = $element.val().trim();
+  console.log(text);
+
+  if (time && text !== "") {
+    console.log(time, text);
+    localStorage.setItem(time, text);
+  }
+});
+
+$(".saveBtn").hover(function () {
+  $(this).addClass("saveBtn:hover");
+});
+
+fetchEvents();
+
+
 
 
